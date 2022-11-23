a61c4f17f4bb14a4593a45b379f97b9847e52e06
robonexx <robertwagar@gmail.com>
2022-11-23 01:49:08 +0100

    working on it

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const words = [
   'bot',
   'car',
   'dog',
+  'and',
+  'god',
+  'arc',
   'house',
   'dance',
   'code',
@@ -28,6 +31,7 @@ let guessArr = [];
 let answerArr = [];
 
 let showArr = [];
+let includedLettersArr = [];
 
 for (let l = 0; l < answer.length; l++) {
   showArr[l] = '_';
@@ -47,15 +51,22 @@ answerArr = answer.split('');
 console.log('this is the word = ' + answerArr.join());
 
 for (let i = 0, j = 0; i < answerArr.length; i++, j++) {
-  if (guessArr.includes(answerArr[i])) {
+  if (guessArr.includes(answerArr[i]) && guessArr[j] !== answerArr[i]) {
+    includedLettersArr.push(answerArr[j]);
     console.log(
-      'these letters are right: ' + guessArr[i] + ' : ' + answerArr[i]
+      'these letters are right: ' +
+        guessArr[j] +
+        ' : ' +
+        answerArr[i] +
+        '\nThese letters are right but in wrong position; ' +
+        '\n' +
+        includedLettersArr
     );
   }
-  if (guessArr[i] == answerArr[i]) {
+  if (guessArr[j] == answerArr[i]) {
     showArr[i] = answerArr[i];
   } else {
-    console.log('these are not right: ' + guessArr[i]);
+    console.log('these are not right: ' + guessArr[j]);
   }
   console.log(showArr);
 }
